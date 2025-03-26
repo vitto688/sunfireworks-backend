34e632ea2cc67c3bc9626b20d8920e8a54441a1e
AlexanderVitto <alexandervitto116@gmail.com>
2025-03-26 08:45:36 +0700

    feat: add allow hosts value

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.sfwarehouse.app','sfwarehouse.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
